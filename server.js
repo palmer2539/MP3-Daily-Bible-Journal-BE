59e78de02e657754af9b777ca0b376ac3c360225
palmer2539 <alexandr.palmer.dev@gmail.com>
2023-01-18 13:52:00 -0500

    connected front and back

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('Express');
-const notes = require('./database/dailynotes');
+const notes = require('./database/dailyEntries');
 const dotenv = require('dotenv');
 
 const app = express();
@@ -9,11 +9,11 @@ app.get('/', (req, res) => {
   res.send("API is active");
 });
 
-app.get('/dailynotes', (req, res) => {
+app.get('/journalentries', (req, res) => {
   res.json(notes);
 });
 
-app.get('/dailynotes/:id', (req, res) => {
+app.get('/journalentries/:id', (req, res) => {
   const single_note = notes.find((n) => n._id === req.params.id);
   res.send(single_note);
 });
