81bab757bd930edc8a6ef4df5465113dc210d316
palmer2539 <alexandr.palmer.dev@gmail.com>
2023-01-22 09:47:02 -0500

    wiring back and front maybe now

--- server.js
+++ server.js
@@ -12,32 +12,14 @@ dotenv.config();
 dbConnection();
 
 app.use(express.json());
-
-app.options('*', cors(corsOptions))
 app.use(cors(corsOptions)) 
 
-// app.options('*', cors())
-
-// app.options('/journalentries/:id', cors()) // enable pre-flight request for DELETE request
-// app.del('/journalentries/:id', cors(), function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for all origins!'})
-// })
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Methods", "GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-
-//  app.use(cors(corsOptions)), 
 
 app.get('/', (req, res) => {
   res.send("API is active");
 });
-app.use('/users', app.use(cors(corsOptions)), userRoutes);
-app.use('/journalentries', app.use(cors(corsOptions)),  entryRoutes);
+app.use('/users', userRoutes);
+app.use('/journalentries',  entryRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
